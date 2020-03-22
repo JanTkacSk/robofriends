ac870ca809836272a9a26901bc62b2d4bd706024
jan.tkac.sk <jan.tkac.sk@gmail.com>
2020-03-22 17:33:24 +0100

    c

--- src/App.js
+++ src/App.js
@@ -9,20 +9,28 @@ class App extends Component {
         this.state = {
 
             robots: robots,
-            seatchfield: ''
+            searchField: ''
         }
     }
 
-    onSearchChange(event){
-        console.log(event.target.value);
+    onSearchChange = (event) => {
+        this.setState({searchField: event.target.value})
+        
     }
     render(){
+        
+        const filteredRobots = this.state.robots.filter(
+            (robot) => {
+                return robot.name.toLowerCase().includes(this.state.searchField.toLowerCase());
+            }
+        )
+
         return(
             <div className='tc'>
     
                 <h1>RoboFriends</h1>
                 <SearchBox searchChange={this.onSearchChange} />
-                <CardList robots={this.state.robots} />
+                <CardList robots={filteredRobots} />
      
     
             </div>
