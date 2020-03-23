316809835dfbda86ce1184264194c5276155cebe
jan.tkac.sk <jan.tkac.sk@gmail.com>
2020-03-23 21:19:59 +0100

    c

--- src/Card.js
+++ src/Card.js
@@ -5,7 +5,7 @@ const card = ({name,email,id}) => {
         <div className="tc bg-light-green dib br3 ma2 grow bw2 shadow-5">
             <img src={`https://robohash.org/${id}?200x200`} alt="robot"></img>
             <div >
-            <h1>{name}</h1>
+            <h2>{name}</h2>
             <p>{email}</p>
 
             </div>
