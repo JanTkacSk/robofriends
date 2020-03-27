1a4a2b29aebde9d106c564854d92927b78f908db
jan.tkac.sk <jan.tkac.sk@gmail.com>
2020-03-27 19:31:54 +0100

    comment

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, {Component} from 'react';
 import CardList from './CardList';
-import {robots} from './robots.js';
 import SearchBox from './SearchBox.js';
 import './App.css';
 
@@ -9,9 +8,17 @@ class App extends Component {
         super()
         this.state = {
 
-            robots: robots,
+            robots: [],
             searchField: ''
         }
+    }
+
+    componentDidMount(){
+        fetch('https://jsonplaceholder.typicode.com/users')
+        .then(response => response.json())
+        .then(response2 => this.setState({robots: response2})
+        )
+        
     }
 
     onSearchChange = (event) => {
@@ -25,6 +32,10 @@ class App extends Component {
                 return robot.name.toLowerCase().includes(this.state.searchField.toLowerCase());
             }
         )
+
+        if (this.state.robots.length === 0){
+            return <h1 className="tc">Loading...</h1>
+        }
 
         return(
             <div className='tc'>
